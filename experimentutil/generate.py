3c86ec94c950b2fb95007223aa580f2e36148ace
Chris <dev@ravngr.com>
2022-07-15 17:31:42 +1000

    Updated documentation for hex_str Added tests for hex_str

--- experimentutil/generate.py
+++ experimentutil/generate.py
@@ -8,9 +8,9 @@ __all__ = [
 
 
 def hex_str(length: int = 8) -> str:
-    """ Generate a random hexadecimal string of a specified length.
+    """ Generate a random hexadecimal string of a specified length in lower case.
 
     :param length: desired string length
-    :return: random hexadecimal string of desired length
+    :return: random hexadecimal string of desired length in lower case
     """
     return ''.join(choice(hexdigits[:16]) for _ in range(length))
---
+++ tests/test_generate.py
@@ -0,0 +1,24 @@
+import unittest
+
+from experimentutil import generate
+
+
+class GeneralTestCase(unittest.TestCase):
+    _CHAR_VALID = set('0123456789abcdef')
+
+    def test_hex_str(self):
+        for test_n in range(100):
+            with self.subTest(f"Test {test_n + 1}"):
+                s = generate.hex_str()
+
+                self.assertEqual(8, len(s))
+                self.assertTrue(set(s).issubset(self._CHAR_VALID))
+
+    def test_hex_str_length(self):
+        for length in range(8, 64 + 1, 8):
+            for test_n in range(10):
+                with self.subTest(f"Length: {length}, Test: {test_n + 1}"):
+                    s = generate.hex_str(length)
+
+                    self.assertEqual(length, len(s))
+                    self.assertTrue(set(s).issubset(self._CHAR_VALID))
