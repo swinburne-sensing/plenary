8814055c83d58696ae09b0c6c0834b16a96ba20a
Chris <dev@ravngr.com>
2022-08-04 16:25:10 +1000

    Added path module, pulled from experimentlib

---
+++ experimentutil/path.py
@@ -0,0 +1,9 @@
+import os
+
+
+def add_path(path: str) -> None:
+    """ Add specified path to the environment PATH variable.
+
+    :param path: path to add to PATH
+    """
+    os.environ['PATH'] = path + os.pathsep + os.environ['PATH']
