1277d7e2991399734e5c97cb1037be2090e75625
Chris <dev@ravngr.com>
2022-08-16 13:03:03 +1000

    Added string tests Added option to disable access to environment/system variables

--- plenary/string.py
+++ plenary/string.py
@@ -17,16 +17,22 @@ _format_system: Mapping[str, str] = {
     'path_home': os.path.expanduser('~'),
     'path_temp': gettempdir(),
     'sys_hostname': getfqdn(),
-    'sys_username': getuser(),
-    **{'env_' + env_var: env_val for env_var, env_val in os.environ.items()}
+    'sys_username': getuser()
 }
 
+_format_env: Mapping[str, str] = {'env_' + env_var: env_val for env_var, env_val in os.environ.items()}
 
-def generate_format(format_spec: str, *args: Any, **kwargs: Any) -> str:
+
+def generate_format(format_spec: str, *args: Any, include_env: bool = True, include_system: bool = True,
+                    **kwargs: Any) -> str:
     """ Format specified string with system and environment variables.
+
+    Inclusion of environment variables (that may contain secrets) is optional but enabled by default.
 
     :param format_spec: formatted string specification
     :param args: additional position format arguments
+    :param include_env: if True allow access to environment variables
+    :param include_system: if True allow access to system variables
     :param kwargs: additional keyword format arguments
     :return: formatted string
     :raises IndexError: on invalid format specification
@@ -46,12 +52,19 @@ def generate_format(format_spec: str, *args: Any, **kwargs: Any) -> str:
         'datetime_utc_console': timestamp_utc.strftime(constant.FORMAT_TIMESTAMP_CONSOLE),
         'datetime_iso': timestamp.isoformat(),
         'datetime_utc_iso': timestamp_utc.isoformat(),
+        'datetime_utc_iso_z': timestamp_utc.isoformat().rsplit('+')[0] + 'Z',
         'timestamp_s': str(int(timestamp.timestamp())),
-        'timestamp_ms': str(int(timestamp.timestamp() * 1000)),
-        'timestamp_us': str(int(timestamp.timestamp() * 1000000))
+        'timestamp_ms': str(int(timestamp.timestamp() * 1e3)),
+        'timestamp_us': str(int(timestamp.timestamp() * 1e6)),
+        'timestamp_ns': str(int(timestamp.timestamp() * 1e9))
     }
 
-    format_mapping.update(_format_system)
+    if include_env:
+        format_mapping.update(_format_env)
+
+    if include_system:
+        format_mapping.update(_format_system)
+
     format_mapping.update(kwargs)
 
-    return format_spec.format(*args, **format_mapping)
+    return format(format_spec, *args, **format_mapping)
---
+++ tests/string_test.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import unittest
+from datetime import datetime
+from unittest.mock import MagicMock, patch
+
+try:
+    # noinspection PyCompatibility
+    from zoneinfo import ZoneInfo
+except ImportError:
+    # noinspection PyPackageRequirements,PyUnresolvedReferences
+    from backports.zoneinfo import ZoneInfo
+
+from plenary import string
+
+
+class StringTestCase(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual('', string.generate_format(''))
+
+    def test_simple(self):
+        self.assertEqual('cake', string.generate_format('cake'))
+
+    def test_args(self):
+        self.assertEqual('cake', string.generate_format('{}', 'cake'))
+        self.assertEqual('potato cake', string.generate_format('{} {}', 'potato', 'cake'))
+
+    def test_kwargs(self):
+        self.assertEqual('cake', string.generate_format('{good}', good='cake'))
+        self.assertEqual('potato cake', string.generate_format('{group} {good}', good='cake', group='potato'))
+
+    def test_datetime(self):
+        t = datetime(2022, 8, 16, 2, 37, 23, 123456, ZoneInfo('Australia/Melbourne'))
+
+        test_list = [
+            ('{date}', '20220816'),
+            ('{date_utc}', '20220815'),
+            ('{time}', '023723'),
+            ('{time_utc}', '163723'),
+            ('{datetime_filename}', '20220816_023723'),
+            ('{datetime_console}', '2022-08-16 02:37:23'),
+            ('{datetime_utc_filename}', '20220815_163723'),
+            ('{datetime_utc_console}', '2022-08-15 16:37:23'),
+            ('{datetime_iso}', '2022-08-16T02:37:23.123456+10:00'),
+            ('{datetime_utc_iso}', '2022-08-15T16:37:23.123456+00:00'),
+            ('{datetime_utc_iso_z}', '2022-08-15T16:37:23.123456Z'),
+            ('{timestamp_s}', '1660581443'),
+            ('{timestamp_ms}', '1660581443123'),
+            ('{timestamp_us}', '1660581443123456'),
+            ('{timestamp_ns}', '1660581443123456000')
+        ]
+
+        # Patch time method for consistent tests
+        with patch('plenary.localtime.now', MagicMock(return_value=t)):
+            for test_format, expected_str in test_list:
+                with self.subTest(f"Testing format {test_format}"):
+                    self.assertEqual(expected_str, string.generate_format(test_format))
+
+    def test_env(self):
+        # PATH should be available in all environments
+        s = string.generate_format('{env_PATH}')
+
+        self.assertIsInstance(s, str)
+
+    def test_env_untrusted(self):
+        with self.assertRaises(KeyError):
+            string.generate_format('{env_PATH}', trust=False)
+
+
+if __name__ == '__main__':
+    unittest.main()
